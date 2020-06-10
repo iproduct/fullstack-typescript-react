158ed41f646908648e7292026ae4990bfb64eb23
Trayan Iliev <office@iproduct.org>
2020-06-10 12:27:32 +0300

    presentation 2 - new exercise added

--- 02-js-intro/js/define-property-get-set.js
+++ 02-js-intro/js/define-property-get-set.js
@@ -1,14 +1,14 @@
 function PositionLogger(aPos) {
-  var position = aPos, positionsLog = [{ val: aPos }];
+  var _position = aPos, positionsLog = [{ val: aPos }];
   // this.position = aPos;
   Object.defineProperty(this, 'position', {
     get: function () {
       // console.log('get position called');
-      return position;
+      return _position;
     },
     set: function (val) {
-      position = val;
-      positionsLog.push({ val: position });
+      _position = val;
+      positionsLog.push({ val: _position });
     },
     enumerable: true
   });
@@ -20,14 +20,16 @@ var pl1 = new PositionLogger(5);
 console.log('Value: ' + pl1.position);
 pl1.position += 20;
 console.log('Value: ' + pl1.position);
-pl1.position -= 10;
+pl1.position -= 10; 
+pl1.position += 50;
 console.log('Value: ' + pl1.position);
+console.log(pl1.positionsLog);
 
 console.log(pl1.getLog());
 
-// for (var key in pl1) {
-//   if (pl1.hasOwnProperty(key)) {
-//     var val = pl1[key];
-//     console.log(key, '->', val);
-//   }
-// }
+for (var key in pl1) {
+  if (pl1.hasOwnProperty(key)) {
+    var val = pl1[key];
+    console.log(key, '->', val);
+  }
+}
