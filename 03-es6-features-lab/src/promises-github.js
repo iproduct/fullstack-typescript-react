bc3cf056e91fe7de4d0ba6ad7f22ac7120e8fec3
Trayan Iliev <office@iproduct.org>
2020-06-12 13:35:45 +0300

    promise demos added

--- 03-es6-features-lab/src/promises-github.js
+++ 03-es6-features-lab/src/promises-github.js
@@ -1,30 +1,25 @@
 document.addEventListener('DOMContentLoaded', init);
 
-function init() {
+function init(){
     return fetch('/users.json')
         .then(resp => resp.json())
-        .then(users => Promise.all(users.map(
-            user => fetch(`https://api.github.com/users/${user.username}`).then(resp => resp.json()))))
-        .then(gitUsers => {
-            console.log(gitUsers);
-            const userImages = gitUsers.map(user => {
-                const userImage = new Image();
-                userImage.src = user.avatar_url;
-                document.getElementById('results').appendChild(userImage);
-                return userImage
-            });
-            return [userImages, gitUsers];
-        }).then(([userImages, gitUsers]) =>
-            Promise.all(userImages.map((userImg, index) => new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    userImg.remove();
-                    resolve(gitUsers[index]);
-                }, (index + 1) * 1000); // timeout
-            })))
-        ).then(gitUsers => {
-            gitUsers.forEach(gitUser => {
-                console.log(`Git user: ${gitUser.name} [${gitUser.login}] - demo finished`);
-            });
+        .then(users => {
+            console.log(users);
+            return fetch(`https://api.github.com/users/${users[0].username}`);
+        }).then(resp => resp.json())
+        .then(gitUser => {
+            console.log(gitUser);
+            const userImage = new Image();
+            userImage.src = gitUser.avatar_url;
+            document.getElementById('results').appendChild(userImage);
+            return [userImage, gitUser];
+        }).then(([userImage, gitUser]) => new Promise((resolve, reject) => {
+            setTimeout(() => {
+                userImage.remove();
+                resolve(gitUser);
+            }, 5000);
+        })).then(gitUser => {
+            console.log(`Git user: ${gitUser.name} [${gitUser.login}] - demo finished`);
         });
-    //https://iproduct:<your-pass>@api.github.com/users/${users[0].username}`
+        //https://iproduct:<your-pass>@api.github.com/users/${users[0].username}`
 }
