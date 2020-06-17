25673cdad679991cf23c41eb2a0e9303be8b0b60
Trayan Iliev <office@iproduct.org>
2020-06-17 11:17:34 +0300

    small changes

--- 06-ts-react-webpack-lab/src/Hello.tsx
+++ 06-ts-react-webpack-lab/src/Hello.tsx
@@ -9,7 +9,7 @@ export class Hello extends React.Component<HelloProps> {
     render() {
         return (
             <div>
-                <h1>Hello from {this.props.compiler} and {this.props.framework}!!!</h1>
+                <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
             </div>
         );
     }
