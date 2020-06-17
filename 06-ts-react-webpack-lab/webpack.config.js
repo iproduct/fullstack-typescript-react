965f9860948739778463d2705f64b79368492891
Trayan Iliev <office@iproduct.org>
2020-06-17 10:36:11 +0300

    webpack config added

---
+++ 06-ts-react-webpack-lab/webpack.config.js
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.tsx'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        compress: true,
+        hot: true,
+        port: 9000
+    },
+    plugins: [
+        new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'index.html'),
+            title: 'Hello React Webpack'
+        })
+    ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.mjs', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node-modules/,
+                use: [
+                    {
+                        loader: 'awesome-typescript-loader'
+                    }
+                ]
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                vendors: false,
+                vendor: {
+                    chunks: 'all',
+                    test: /node-modules/
+                }
+            }
+        }
+    }
+}
