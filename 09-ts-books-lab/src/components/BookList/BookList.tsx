9ef89ca40c55a8d6f0478e3b2c2038ffe3f835aa
Trayan Iliev <office@iproduct.org>
2020-06-23 14:28:56 +0300

    header and footer added

---
+++ 09-ts-books-lab/src/components/BookList/BookList.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Book } from '../../model/book.model';
+import { ReactElement } from 'react';
+
+interface Props {
+    books: Book[];
+}
+
+export function BookList(props: Props): ReactElement<Props> {
+    return (
+      <div>
+        
+      </div>
+    );
+};
