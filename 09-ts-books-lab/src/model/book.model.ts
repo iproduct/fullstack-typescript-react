ecf1a7973a253ad3463b83da83c830e3cd664551
Trayan Iliev <office@iproduct.org>
2020-06-23 13:57:38 +0300

    repository added

---
+++ 09-ts-books-lab/src/model/book.model.ts
@@ -0,0 +1,17 @@
+import { Indentifiable, IdType } from '../shared/shared-types';
+export interface IBook extends Indentifiable {
+    title: string;
+    subtitle?: string;
+    authors: string;
+    frontPage: string;
+}
+
+export class Book implements IBook {
+    id: IdType = 0;
+    constructor(
+        public title: string,
+        public authors: string,
+        public frontPage: string,
+        public subtitle?: string,
+        ) {}
+}
