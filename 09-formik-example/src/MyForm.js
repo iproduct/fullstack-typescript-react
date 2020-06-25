915dc152b23a1f84aaac572ffb27f1ea2ed8814a
Trayan Iliev <office@iproduct.org>
2020-06-25 09:58:02 +0300

    externalized myform form

---
+++ 09-formik-example/src/MyForm.js
@@ -0,0 +1,54 @@
+
+import React from "react";
+import { MoreResources, DisplayFormikState } from "./helper";
+
+export const MyForm = (props) => {
+    const {
+      values,
+      touched,
+      errors,
+      dirty,
+      isSubmitting,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      handleReset
+    } = props;
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email" style={{ display: "block" }}>
+          Email
+        </label>
+        <input
+          id="email"
+          placeholder="Enter your email"
+          type="text"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className={
+            errors.email && touched.email
+              ? "text-input error"
+              : "text-input"
+          }
+        />
+        {errors.email && touched.email && (
+          <div className="input-feedback">{errors.email}</div>
+        )}
+
+        <button
+          type="button"
+          className="outline"
+          onClick={handleReset}
+          disabled={!dirty || isSubmitting}
+        >
+          Reset
+        </button>
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+
+        <DisplayFormikState {...props} />
+      </form>
+    );
+  }
--- 09-formik-example/src/index.js
+++ 09-formik-example/src/index.js
@@ -6,6 +6,7 @@ import React from "react";
 import { render } from "react-dom";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import {MyForm} from "./MyForm";
 
 const App = () => (
   <div className="app">
@@ -33,56 +34,7 @@ const App = () => (
           .required("Required")
       })}
     >
-      {props => {
-        const {
-          values,
-          touched,
-          errors,
-          dirty,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          handleReset
-        } = props;
-        return (
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="email" style={{ display: "block" }}>
-              Email
-            </label>
-            <input
-              id="email"
-              placeholder="Enter your email"
-              type="text"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={
-                errors.email && touched.email
-                  ? "text-input error"
-                  : "text-input"
-              }
-            />
-            {errors.email && touched.email && (
-              <div className="input-feedback">{errors.email}</div>
-            )}
-
-            <button
-              type="button"
-              className="outline"
-              onClick={handleReset}
-              disabled={!dirty || isSubmitting}
-            >
-              Reset
-            </button>
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-
-            <DisplayFormikState {...props} />
-          </form>
-        );
-      }}
+      {props => <MyForm {...props}/>}
     </Formik>
 
     <MoreResources />
