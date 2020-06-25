5f86d7bebb42afe18b9af3710e8e9441a5ae3c27
Trayan Iliev <office@iproduct.org>
2020-06-25 16:38:00 +0300

    react ref demo lab initial commit

---
+++ 10-react-refs-labs/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UncontrolledForm from './UncontrolledForm';
+import { UncontrolledFunctionForm } from './UncontrolledFunctionForm copy';
+
+function App() {
+  return (
+    <UncontrolledFunctionForm />
+  );
+}
+
+export default App;
---
+++ 10-react-refs-labs/src/UncontrolledFunctionForm copy.tsx
@@ -0,0 +1,30 @@
+import React, { Component, useRef } from 'react'
+
+interface Props {
+}
+interface State {
+}
+
+export function UncontrolledFunctionForm() {
+    const nameRef = useRef<HTMLInputElement>(null);
+    const experienceRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        alert(`A name: '${nameRef.current?.value}' with experience: ${experienceRef.current?.value} was sumitted.`);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Name:
+            <input type="text" ref={nameRef} />
+            </label>
+            <label>
+                Experience:
+                <input type="text" ref={experienceRef} />
+            </label>
+            <input type="submit" value="Submit" />
+            <input type="buton" onClick={(ev) => {experienceRef.current?.focus()}} value="Focuse Experience" />
+        </form>
+    );
+}
