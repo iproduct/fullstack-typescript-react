1acee6dc6e56ae1f453aa57e8d0e91bc853c3f4f
Trayan Iliev <office@iproduct.org>
2020-06-30 09:03:47 +0300

    ts node project added

---
+++ 15-ts-node/src/server.ts
@@ -0,0 +1,9 @@
+import * as express from "express";
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+     console.log(`Server is running in http://localhost:${PORT}`)
+})
