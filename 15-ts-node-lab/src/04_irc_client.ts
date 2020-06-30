3d6e8680bedfad6e34305c7484c51896be39c8a5
Trayan Iliev <office@iproduct.org>
2020-06-30 16:08:59 +0300

    irc demo added

--- 15-ts-node-lab/src/04_irc_client.ts
+++ 15-ts-node-lab/src/04_irc_client.ts
@@ -1,20 +1,44 @@
+import * as irc from 'irc';
 import * as Rx from 'rxjs';
 import { from, interval, zip } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { EventEmitter } from 'events';
-import e = require('express');
+import { map, filter } from 'rxjs/operators';
+import * as process from 'process';
 
-const emitter = new EventEmitter();
-
-emitter.on('myevent', (payload, status) => {
-    console.log(`Event received: ${JSON.stringify(payload)}, ${JSON.stringify(status)}`);
+const client = new irc.Client('irc.freenode.net', 'trayan_bot', {
+    channels: ['#course-node']
 });
 
-const data = from(['Hello', 'Reactive', 'Extensions', 'JavaScript', 'from', 'TypeScript']);
-const int = interval(1000);
+client.on('error', (err) => {
+    console.error(err);
+});
+
+client.on('message', (from, to, message) => {
+    console.log(`Message received: ${from} -> ${to} : ${message}`);
+});
+
+const data = from(['', '', '', 'Hello', 'Reactive', 'Extensions', 'JavaScript', 'from', 'TypeScript']);
+const int = interval(3000);
 
 const result = zip(data, int).pipe(
+    filter(([text, num]) => text.length > 0),
     map(([text, num]) => `${num}: ${text} [${new Date().getTime()}]`)
 );
 
-result.subscribe(next => emitter.emit('myevent', {text: next}));
+client.on('connect', () => {
+    result.subscribe(next => client.say('#course-node', next),
+    err => console.error(err),
+    () => {
+        // client.part('#course-node');
+        console.log('All messages sent successfully.');
+    });
+});
+
+const finish = () => {
+     client.part('#course-node','Bye from trayans bot!', undefined);
+     console.log('Demo finished');
+     process.exit(0);
+};
+setTimeout(finish, 120000)
+
+
+
