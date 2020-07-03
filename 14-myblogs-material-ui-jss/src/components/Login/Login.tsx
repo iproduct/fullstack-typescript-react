e0450f02b9688de4f8d698b101c4717e12f9ac3d
Trayan Iliev <office@iproduct.org>
2020-07-03 22:12:22 +0300

    unused imports cleaned up

--- 14-myblogs-material-ui-jss/src/components/Login/Login.tsx
+++ 14-myblogs-material-ui-jss/src/components/Login/Login.tsx
@@ -1,17 +1,15 @@
-import React, { ReactElement } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import React, { ReactElement } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
--- 14-myblogs-material-ui-jss/src/components/MaterialField/MaterialField.tsx
+++ 14-myblogs-material-ui-jss/src/components/MaterialField/MaterialField.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-import { Field, ErrorMessage, getIn, connect, FormikContextType, FieldProps } from 'formik';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-// import './MaterialField.css';
+import { connect, Field, FieldProps } from 'formik';
+import React from 'react';
 
 interface MaterialFiledProps {
     name: string;
@@ -11,27 +9,7 @@ interface MaterialFiledProps {
 }
 
 function MaterialFiled({ name, label, rowsMax = 1 }: MaterialFiledProps) {
-    // const props = useFormikContext();
-
-    // const {
-    //     values,
-    //     errors,
-    //     touched,
-    //     handleChange,
-    //     setFieldTouched
-    // } = formik;
-    // const change = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     event.persist();
-    //     handleChange(event);
-    //     setFieldTouched(name, true, true);
-    // };
-    // const fieldValue = getIn(values, name);
-    // const fieldError = getIn(errors, name);
-    // const fieldTouched = getIn(touched, name);
-    // let classes = displayAs === 'textarea' ? 'materialize-textarea ': '';
-    // classes += error && touched ? 'invalid' : 'valid';
     return (
-        // <div className="input-field col s12">
         <Field name={name}>
             {({
                 field, // { name, value, onChange, onBlur }
@@ -51,23 +29,6 @@ function MaterialFiled({ name, label, rowsMax = 1 }: MaterialFiledProps) {
                 )
             }
         </Field>
-        // <TextField
-        //     id={name}
-        //     name={name}
-        //     helperText={fieldTouched ? fieldError : ""}
-        //     error={fieldTouched && Boolean(fieldError)}
-        //     label={label}
-        //     value={fieldValue}
-        //     onChange={change}
-        //     fullWidth
-        // />
-        // <Field type="text" as={displayAs} className={classes} name={name} />
-        // <TextField id="standard-basic" label="Standard" />
-        // <label className={error && touched ? 'active field-error' : 'active'} htmlFor={name}>
-        //     {label}
-        // </label>
-        // <ErrorMessage className="field-error" name={name} component="div" />
-        // </div>
     );
 };
 
