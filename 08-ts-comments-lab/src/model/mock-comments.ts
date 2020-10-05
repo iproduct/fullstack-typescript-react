50258fe1306e127e812c12f962978438b15e5ce6
Trayan Iliev <office@iproduct.org>
2020-10-05 17:26:02 +0300

    json sample data changed

--- 08-ts-comments-lab/src/model/mock-comments.ts
+++ 08-ts-comments-lab/src/model/mock-comments.ts
@@ -5,5 +5,5 @@ export default [
     new Comment('Dimitar Ivanov', 'React is *great*!'),
     new Comment('Hristo Vasilev', '*abcd*'),
     new Comment('Ben', 'Do you like **TSX**?'),
-    new Comment('Vasil', 'Is Typescript easy?'),
+    new Comment('Filip', 'Is Typescript easy?'),
 ];
