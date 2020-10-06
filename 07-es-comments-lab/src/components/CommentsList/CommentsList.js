0829e43a8a04d26c3e9399752f6a54fbb2d47c33
Trayan Iliev <office@iproduct.org>
2020-10-06 13:08:13 +0300

    CommentsList component created

---
+++ 07-es-comments-lab/src/components/CommentsList/CommentsList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function CommentsList(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+CommentsList.propTypes = {
+
+}
+
+export default CommentsList
+
