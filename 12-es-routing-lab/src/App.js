23615cc69f5521de8f1a5ee160b61b6c59c77196
Trayan Iliev <office@iproduct.org>
2020-10-08 17:12:14 +0300

    dynamic menus demo

--- 12-es-routing-lab/src/App.js
+++ 12-es-routing-lab/src/App.js
@@ -1,6 +1,20 @@
-Rimport React from 'react';
+import React from 'react';
 import { Link, Route, BrowserRouter as Router, Switch, useParams, withRouter } from 'react-router-dom';
 import './App.css';
+
+const blogsRepo = [
+  {
+    title: 'Components',
+    text: 'All about Components - can be functions.',
+    comments: [{id:'easy', text: 'Comps are reasy.'}, {id:'hard', text: 'Comps are hard.'}]
+  },
+  {
+    title: 'HOCs',
+    text: 'All about HOCs',
+    comments: [{id:'easy', text: 'HOCs are reasy.'}, {id:'hard', text: 'HOCs are hard.'}]
+  }
+];
+
 
 function App() {
   return (
@@ -15,8 +29,7 @@ function App() {
             <li><Link to="/users/atanas">Users/atanas</Link></li>
             <li><Link to="/topics/components">React Components</Link></li>
             <li><Link to="/topics/props-vs-state">Props vs. State</Link></li>
-            <li><Link to="/topics/hocs/comments/seems-advanced">React HOCs</Link></li>
-            <li><Link to="/topics/hocs/comments/easy-thing">React HOCs</Link></li>
+            <li><Link to="/topics/hocs">React HOCs with Comment 1</Link></li>
           </ul>
         </nav>
 
@@ -39,20 +52,13 @@ const Topics = withRouter(
       <React.Fragment>
         <h2>My Blog</h2>
         <h3>{match.params.topicId}</h3>
+        <h4>Comments:</h4>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+        </ul>
         <Switch>
           <Route path={`${match.path}/comments/:commentId`}>
-            {
-              () => {
-                const { commentId } = useParams();
-                return (
-                  <React.Fragment>
-                    <hr />
-                    <h3>Comment Details for {match.params.topicId}:</h3>
-                    <p>Comment Text: {}</p>
-                  </React.Fragment>
-                );
-              }
-            }
+            <CommentDetails />
           </Route>
         </Switch>
       </React.Fragment>
@@ -75,6 +81,17 @@ function Users() {
     <React.Fragment>
       <h2>Users</h2>
       <h2>UserID: {userId}</h2>
+    </React.Fragment>
+  );
+}
+
+const CommentDetails = () => {
+  const { commentId, topicId } = useParams();
+  return (
+    <React.Fragment>
+      <hr />
+      <h3>Comment Details for {topicId}</h3>
+      <p>Comment Text: {commentId}</p>
     </React.Fragment>
   );
 }
