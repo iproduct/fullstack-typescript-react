3d3ce3ee60e7c727a950c2d33ea2868834bd33cc
Trayan Iliev <office@iproduct.org>
2020-10-08 17:46:02 +0300

    dynamic menu and route building

--- 12-es-routing-lab/src/App.js
+++ 12-es-routing-lab/src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 
 const blogsRepo = [
   {
+    id: 'coponents',
     title: 'Components',
     text: 'All about Components - can be functions.',
-    comments: [{id:'easy', text: 'Comps are reasy.'}, {id:'hard', text: 'Comps are hard.'}]
+    comments: [{id:'comp-easy', text: 'Comps are reasy.'}, {id:'comp-hard', text: 'Comps are hard.'}]
   },
   {
+    id: 'hocs',
     title: 'HOCs',
     text: 'All about HOCs',
-    comments: [{id:'easy', text: 'HOCs are reasy.'}, {id:'hard', text: 'HOCs are hard.'}]
+    comments: [{id:'hoc-easy', text: 'HOCs are reasy.'}, {id:'hoc-hard', text: 'HOCs are hard.'}]
   }
 ];
 
@@ -27,9 +29,7 @@ function App() {
             <li><Link to="/users/pesho">Users/pesho</Link></li>
             <li><Link to="/users/gosho">Users/gosho</Link></li>
             <li><Link to="/users/atanas">Users/atanas</Link></li>
-            <li><Link to="/topics/components">React Components</Link></li>
-            <li><Link to="/topics/props-vs-state">Props vs. State</Link></li>
-            <li><Link to="/topics/hocs">React HOCs with Comment 1</Link></li>
+            { blogsRepo.map(blog => (<li key={blog.id}><Link to={`/topics/${blog.id}`}>{blog.title}</Link></li>)) }
           </ul>
         </nav>
 
@@ -48,13 +48,15 @@ export default App;
 const Topics = withRouter(
   (props) => {
     const { match, location, history } = props;
+    const post = blogsRepo.find(post => post.id === match.params.topicId);
     return (
       <React.Fragment>
         <h2>My Blog</h2>
-        <h3>{match.params.topicId}</h3>
+        <h3>{post.title}</h3>
+        <p>{post.text}</p>
         <h4>Comments:</h4>
         <ul>
-            <li><Link to="/">Home</Link></li>
+            { post.comments.map(c => (<li key={c.id}><Link to={`${match.url}/comments/${c.id}`}>{c.id}</Link></li>)) }
         </ul>
         <Switch>
           <Route path={`${match.path}/comments/:commentId`}>
@@ -87,11 +89,14 @@ function Users() {
 
 const CommentDetails = () => {
   const { commentId, topicId } = useParams();
+  const comment = blogsRepo
+        .find(blog => blog.id === topicId).comments.find(c => c.id === commentId);
   return (
     <React.Fragment>
       <hr />
       <h3>Comment Details for {topicId}</h3>
-      <p>Comment Text: {commentId}</p>
+      <p>Comment ID: {commentId}</p>
+      <p>Comment Text: {comment.text}</p>
     </React.Fragment>
   );
 }
