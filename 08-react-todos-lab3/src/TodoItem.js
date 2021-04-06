865745762bda8e6741e1af1396c8ce0d1b99d04f
Trayan Iliev <office@iproduct.org>
2021-04-06 20:22:37 +0300

    aditional styling + statuses as text

--- 08-react-todos-lab3/src/TodoItem.js
+++ 08-react-todos-lab3/src/TodoItem.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import { TodoStatus } from './todo-model'
+import './TodoItem.css'
 
 export const TodoItem = ({todo}) => {
     return (
         <div className="TodoItem">
             <span className="TodoItem-text">
                 <span className="TodoItem-id">
-                    {todo.id}
+                    {todo.id}.
                 </span>
                 {todo.text}
             </span>
             <span className="TodoItem-right">
                 <span className="TodoItem-status">
-                    {todo.status}
+                    {TodoStatus[todo.status]}
                 </span>
             </span>
         </div>
