8f3cf6aa1d14887581e094caa64e6cf3c9c68f50
Trayan Iliev <office@iproduct.org>
2021-04-13 02:50:02 +0300

    css styling improved

--- 08-google-books-lab1/src/App.js
+++ 08-google-books-lab1/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import React from 'react';
-import Nav from './components/Nav/Nav'
+import Nav from './components/Nav/Nav';
+import BookService from './service/book-service';
 
 function App() {
+  function handleSerachBooks(searchText) {
+    BookService.loadBooks(searchText);
+  }
   return (
     <React.Fragment>
-      <Nav />
+      <Nav onSearchBooks={handleSerachBooks} />
       <div className="section no-pad-bot">
         <div className="container">
-          <h1>Your Favourite Books</h1>
+          <h1 class="header orange-text">Your Favourite Books</h1>
         </div>
       </div>
     </React.Fragment>
--- 08-google-books-lab1/src/components/Nav/Nav.js
+++ 08-google-books-lab1/src/components/Nav/Nav.js
@@ -1,45 +1,71 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
 
-export default function Nav() {
+export default function Nav({ onSearchBooks }) {
+    const [searchText, setSearchText] = useState('');
+    // const labelEl = useRef(null);
+    function submitSearch(event) {
+        const text = searchText.trim();
+        if (text.length > 0) {
+            setSearchText('');
+            // labelEl.current.className = '';
+            onSearchBooks(text);
+        }
+    }
+    function handleTextChanged(event) {
+        setSearchText(event.target.value);
+    }
     return (
         <React.Fragment>
-            <nav className="light-blue lighten-1" role="navigation">
-                <a href="#" data-target="slide-out" className="sidenav-trigger">
-                    <i className="large material-icons">menu</i>
-                </a>
+            <nav className="Nav light-blue lighten-1" role="navigation">
                 <div className="nav-wrapper container">
-                    <a id="logo-container" href="#">
+                    <a id="logo-container" className="brand-logo hide-on-med-and-down" href="#">
                         <i className="large material-icons">menu_book</i>
                     </a>
+
+                    <ul className="right col12">
+                        <li>
+                            <form className="col">
+                                <div className="row">
+                                    <div className="input-field col s9">
+                                        <input id="book-keywords" type="text"
+                                            value={searchText} onChange={handleTextChanged} />
+                                        <label htmlFor="book-keywords">Search keywords</label>
+                                    </div>
+
+                                    <div className="Nav-button col s2 waves-effect btn light-blue" onClick={submitSearch}>
+                                        <i className="material-icons">search</i>
+                                    </div>
+                                </div>
+                            </form>
+                        </li>
+                    </ul>
                     <ul className="right hide-on-med-and-down">
-                        <li><a href="#">Navbar Link</a></li>
+                        <li><a href="#">Second Link</a></li>
+                        <li>
+                            <a className="waves-effect" href="#!">Third Link With Waves</a>
+                        </li>
                     </ul>
 
                     <ul id="nav-mobile" className="sidenav">
-                        <li><a href="#">Navbar Link</a></li>
+                        <li><div className="user-view">
+                            <div className="background">
+                                <img src="img/old-books.jpg" alt="books app" />
+                            </div>
+                            <a href="#name"><span className="white-text name">John Doe</span></a>
+                            <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
+                        </div>
+                        </li>
+                        <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
+                        <li><a href="#!">Second Link</a></li>
+                        <li><div className="divider"></div></li>
+                        <li><a className="subheader">Subheader</a></li>
+                        <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
                     </ul>
                     <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                 </div>
             </nav>
-            <ul id="slide-out" className="sidenav">
-                <li><div className="user-view">
-                    <div className="background">
-                        <img src="img/old-books.jpg" alt="books app" />
-                    </div>
-                    <a href="#name"><span className="white-text name">John Doe</span></a>
-                    <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
-                </div>
-                </li>
-                <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
-                <li><a href="#!">Second Link</a></li>
-                <li><div className="divider"></div></li>
-                <li><a className="subheader">Subheader</a></li>
-                <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
-            </ul>
-
-
         </React.Fragment>
     )
 }
