73a374c4ef5136a5d7a2ebfba9d376e87b378d1e
Trayan Iliev <office@iproduct.org>
2021-04-12 20:49:12 +0300

    navigation tuned

--- 08-google-books-lab1/src/components/Nav/Nav.js
+++ 08-google-books-lab1/src/components/Nav/Nav.js
@@ -1,9 +1,28 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React from 'react';
+import './Nav.css';
 
 export default function Nav() {
     return (
         <React.Fragment>
+            <nav className="light-blue lighten-1" role="navigation">
+                <a href="#" data-target="slide-out" className="sidenav-trigger">
+                    <i className="large material-icons">menu</i>
+                </a>
+                <div className="nav-wrapper container">
+                    <a id="logo-container" href="#">
+                        <i className="large material-icons">menu_book</i>
+                    </a>
+                    <ul className="right hide-on-med-and-down">
+                        <li><a href="#">Navbar Link</a></li>
+                    </ul>
+
+                    <ul id="nav-mobile" className="sidenav">
+                        <li><a href="#">Navbar Link</a></li>
+                    </ul>
+                    <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                </div>
+            </nav>
             <ul id="slide-out" className="sidenav">
                 <li><div className="user-view">
                     <div className="background">
@@ -19,7 +38,7 @@ export default function Nav() {
                 <li><a className="subheader">Subheader</a></li>
                 <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
             </ul>
-            <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+
 
         </React.Fragment>
     )
