d3022120226d8de637a90f892e3d00e36a0b5cb1
Trayan Iliev <office@iproduct.org>
2021-04-13 21:37:12 +0300

    books service added

---
+++ 09-google-books-react-lab3/src/service/book.service.js
@@ -0,0 +1,36 @@
+import { Repository } from "../dao/repository";
+import { Book } from "../model/book.model";
+import MOCK_BOOKS from "../model/mock-books";
+
+export const GOOGLE_BOOKS_API_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+class BookService {
+    repo = new Repository();
+    constructor(apiUrl) {
+        this.apiUrl = apiUrl;
+        MOCK_BOOKS.forEach(book => this.repo.create(book));
+    }
+
+    async getAllBooks() {
+        return this.repo.findAll();
+    }
+
+    async searchBooks(searchTerms) {
+        console.log(`Searching for: ${searchTerms}`);
+        const foundBooksResp = await fetch(this.apiUrl + encodeURIComponent(searchTerms))
+        const foundBooks = await foundBooksResp.json();
+        console.log(foundBooks);
+        return foundBooks.map(item => new Book(
+            item.id,
+            item.volumeInfo.title,
+            item.volumeInfo.authors,
+            item.volumeInfo.imageLinks?.thumbnail,
+            item.volumeInfo.subtitle,
+            item.volumeInfo.categories,
+            searchTerms,
+            item.volumeInfo.description
+        ));
+    }
+}
+
+export default new BookService(GOOGLE_BOOKS_API_URL);
